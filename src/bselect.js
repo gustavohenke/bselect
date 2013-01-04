be812b5ee222a146e2baa210abf2c4feb4a36f0f
Gustavo Henke <gustavo@injoin.com.br>
2013-01-04 15:03:15 -0200

    Starting with jQuery coding style

--- src/bselect.js
+++ src/bselect.js
@@ -9,105 +9,105 @@
  * @author   Gustavo Henke <gustavo@injoin.com.br>
  * @requires jQuery
  */
-(function($, undefined) {
+(function( $, undefined ) {
 	"use strict";
 
 	var elements = 0;
 	var methods = {
 		// Get/set options of the component
-		option : function(option, value) {
+		option: function( option, value ) {
 			var curr = this.data("bselect").options || {};
 
-			if (typeof option === "string" && option[0] !== "_") {
-				if (value === undefined) {
+			if ( typeof option === "string" && option[0] !== "_" ) {
+				if ( value === undefined ) {
 					return curr[option];
 				} else {
 					curr[option] = value;
 					return this;
 				}
-			} else if ($.isPlainObject(option)) {
-				return this.data("bselect", $.extend(curr, option));
+			} else if ( $.isPlainObject(option) ) {
+				return this.data( "bselect", $.extend( curr, option ) );
 			}
 
 			return curr;
 		},
 
 		// Retrieve the BSelect container
-		element : function() {
+		element: function() {
 			return this.data("bselect").element;
 		},
 
 		// Retrieve the currently selected value
-		value : function() {
+		value: function() {
 			return this.data("bselect").value;
 		},
-		toggle : function(e) {
-			var bselect = _callMethod(this, "element");
+		toggle: function(e) {
+			var bselect = _callMethod( this, "element" );
 
-			if (bselect.find(".dropdown-menu").is(":hidden")) {
-				_callMethod(this, "show");
+			if ( bselect.find(".dropdown-menu").is(":hidden") ) {
+				_callMethod( this, "show" );
 				return (e instanceof $.Event) ? false : this;
 			} else {
-				_callMethod(this, "hide");
+				_callMethod( this, "hide" );
 			}
 
 			return this;
 		},
-		show : function() {
+		show: function() {
 			var searchInput;
-			var bselect = _callMethod(this, "element");
+			var bselect = _callMethod( this, "element" );
 
-			bselect.find(".dropdown-menu").slideDown(_callMethod(this, "option", "animationDuration"), function() {
-				adjustDropdownHeight(bselect);
+			bselect.find(".dropdown-menu").slideDown( _callMethod( this, "option", "animationDuration" ), function() {
+				adjustDropdownHeight( bselect );
 			});
 
 			// The following class will allow us to show that nice inset shadow in .dropdown-toggle
-			bselect.addClass('open');
+			bselect.addClass("open");
 
 			// Adjust the size of the search input to match the container inner width
 			searchInput = bselect.find(".bselect-search");
-			searchInput.innerWidth(searchInput.parent().width() - searchInput.next().outerWidth());
+			searchInput.innerWidth( searchInput.parent().width() - searchInput.next().outerWidth() );
 
 			return this;
 		},
-		hide : function(clear) {
-			var options = _callMethod(this, "option"),
-				bselect = _callMethod(this, "element");
+		hide: function( clear ) {
+			var options = _callMethod( this, "option" ),
+				bselect = _callMethod( this, "element" );
 
 			clear = clear === undefined ? true : clear;
 
-			bselect.find(".dropdown-menu").slideUp(options.animationDuration);
+			bselect.find(".dropdown-menu").slideUp( options.animationDuration );
 			bselect.removeClass('open');
 
 			// Clear the search input and the results, if that's case
-			if (clear && options.clearSearchOnExit) {
-				_callMethod(this, "clearSearch");
+			if ( clear && options.clearSearchOnExit ) {
+				_callMethod( this, "clearSearch" );
 			}
 
 			return this;
 		},
 
 		// Clear the search input and reset the dropdown
-		clearSearch : function() {
-			var bselect = _callMethod(this, "element");
+		clearSearch: function() {
+			var bselect = _callMethod( this, "element" );
 
 			bselect.find(".bselect-search").val("");
 			bselect.find("li").show();
 
-			adjustDropdownHeight(bselect);
+			adjustDropdownHeight( bselect );
 			return this;
 		},
-		select : function(arg) {
+		select: function(arg) {
 			var $elem, val;
-			var options = _callMethod(this, "option"),
-				bselect = _callMethod(this, "element");
+			var options = _callMethod( this, "option" ),
+				bselect = _callMethod( this, "element" );
 
-			if (arg instanceof $.Event) {
-				$elem = $(arg.currentTarget);
+			if ( arg instanceof $.Event ) {
+				$elem = $( arg.currentTarget );
 			} else {
-				$elem = bselect.find("li").eq(arg);
+				$elem = bselect.find("li").eq( arg );
 
-				if (!$elem.length) {
+				if ( !$elem.length ) {
 					return this;
 				}
 			}
@@ -118,55 +118,55 @@
 			val = $elem.addClass("active").data("value");
 			this.data("bselect").value = val;
 
-			bselect.find(".bselect-label").text($elem.text());
-			_callMethod(this, "hide");
+			bselect.find(".bselect-label").text( $elem.text() );
+			_callMethod( this, "hide" );
 
-			if (options.synchronizeElement) {
-				this.val(val);
+			if ( options.synchronizeElement ) {
+				this.val( val );
 			}
 
 			// Trigger the select event...
-			if (typeof options.select === "function") {
-				options.select.call(this, val, $elem);
+			if ( typeof options.select === "function" ) {
+				options.select.call( this, val, $elem );
 			}
 
 			return this;
 		},
 
 		// Searches every item in the list for the given text
-		search : function(arg) {
+		search: function(arg) {
 			var listItems, i;
-			var options = _callMethod(this, "option"),
+			var options = _callMethod( this, "option" ),
 				searched = (arg instanceof $.Event) ? arg.target.value : arg,
-				bselect = _callMethod(this, "element");
+				bselect = _callMethod( this, "element" );
 
 			// Avoid searching for nothing
-			if (searched === "") {
-				_callMethod(this, "clearSearch");
+			if ( searched === "" ) {
+				_callMethod( this, "clearSearch" );
 			}
 
-			if (!(arg instanceof $.Event)) {
-				bselect.find(".bselect-search").val(searched);
+			if ( !(arg instanceof $.Event) ) {
+				bselect.find(".bselect-search").val( searched );
 			}
 
 			// Same search/few chars? We won't search then!
-			if ((searched === options.lastSearch) || (searched.length < options.minSearchInput)) {
+			if ( ( searched === options.lastSearch ) || ( searched.length < options.minSearchInput ) ) {
 				return;
 			}
 
 			listItems = bselect.find("li").hide();
-			for (i = 0; i < listItems.length; i++) {
-				if (listItems[i].textContent.toLowerCase().indexOf(searched.toLowerCase()) > -1) {
-					$(listItems[i]).show();
+			for ( i = 0; i < listItems.length; i++ ) {
+				if ( listItems[i].textContent.toLowerCase().indexOf( searched.toLowerCase() ) > -1 ) {
+					$( listItems[i] ).show();
 				}
 			}
 
-			adjustDropdownHeight(listItems.end());
+			adjustDropdownHeight( listItems.end() );
 			return this;
 		},
 		destroy : function() {
-			var bselect = _callMethod(this, "element");
-			this.data("bselect", null);
+			var bselect = _callMethod( this, "element" );
+			this.data( "bselect", null );
 
 			bselect.remove();
 			this.show();
@@ -175,37 +175,37 @@
 	var bootstrapButtonSizes = ["mini", "small", "large"];
 
 	// Helper function that will call an BSelect method in the context of $elem
-	function _callMethod($elem, method) {
-		if (methods[method] !== undefined) {
-			return methods[method].apply($elem, Array.prototype.slice.call(arguments, 2));
+	function _callMethod( $elem, method ) {
+		if ( methods[method] !== undefined ) {
+			return methods[method].apply( $elem, Array.prototype.slice.call( arguments, 2 ) );
 		}
 
 		return $elem;
 	}
 
-	function getPlaceholder($elem) {
-		return _callMethod($elem, "option", "placeholder") ||
+	function getPlaceholder( $elem ) {
+		return _callMethod( $elem, "option", "placeholder" ) ||
 				$elem.data("placeholder") ||
 				$.bselect.i18n.selectAnOption;
 	}
 
-	function adjustDropdownHeight($elem) {
+	function adjustDropdownHeight( $elem ) {
 		var list = $elem.find(".dropdown-list"),
 			len = list.find("li:visible").length;
 
-		list.innerHeight(parseInt(list.css("line-height"), 10) * 1.5 * (len < 5 ? len : 5));
+		list.innerHeight( parseInt( list.css("line-height"), 10 ) * 1.5 * ( len < 5 ? len : 5 ) );
 	}
 
-	function setup(elem, options) {
+	function setup( elem, options ) {
 		var caret, label, container, html;
-		var $elem = $(elem),
+		var $elem = $( elem ),
 			btn = $("<button class='btn' />");
 
 		// First of, let's build the base HTML of BSelect
 		html = "<div class='bselect btn-group' id='bselect-" + (++elements) + "'>";
 		html += "<div class='dropdown-menu'>";
 
-		if (options.searchInput) {
+		if ( options.searchInput ) {
 			html += "<div class='input-append'>" +
 						"<input type='text' class='bselect-search' />" +
 						"<span class='add-on'><i class='icon-search'></i></span>" +
@@ -221,74 +221,73 @@
 		});
 		html += "</ul></div></div>";
 
-		container = $elem.after(html).next();
+		container = $elem.after( html ).next();
 
 		// Save some precious data in the original select now, as we have the container in the DOM
 		$elem.data("bselect", {
-			options : options,
-			element : container
+			options: options,
+			element: container
 		});
 
-		if (options.size !== "normal" && bootstrapButtonSizes.indexOf(options.size) > -1) {
-			btn.addClass("btn-" + options.size);
+		if ( options.size !== "normal" && bootstrapButtonSizes.indexOf( options.size ) > -1 ) {
+			btn.addClass( "btn-" + options.size );
 		}
 
-		label = btn.clone().addClass("bselect-label").text(getPlaceholder($elem));
+		label = btn.clone().addClass("bselect-label").text( getPlaceholder( $elem ) );
 		caret = btn.addClass("dropdown-toggle").html("<span class='caret'></span>");
-		container.prepend(caret).prepend(label);
+		container.prepend( caret ).prepend( label );
 
-		label.outerWidth($elem.outerWidth() - caret.outerWidth());
+		label.outerWidth( $elem.outerWidth() - caret.outerWidth() );
 		
 		// Hide this ugly select!
 		$elem.hide();
 
 		// Event binding
-		$(document).click(function(e) {
-			if (container.find(".dropdown-menu").is(":visible") &&
-				!$('.dropdown-menu, .dropdown-menu *', container).find(e.target).length) {
-				_callMethod($elem, "hide");
+		$( document ).click(function(e) {
+			if ( container.find(".dropdown-menu").is(":visible") && !$( ".dropdown-menu, .dropdown-menu *", container ).find( e.target ).length ) {
+				_callMethod( $elem, "hide" );
 			}
 		});
 
 		// Showing the options list
-		caret.click($.proxy(methods.toggle, $elem));
+		caret.click( $.proxy( methods.toggle, $elem ) );
 		if (options.showOn === "both") {
-			label.click($.proxy(methods.toggle, $elem));
+			label.click( $.proxy( methods.toggle, $elem ) );
 		}
 
 		container.find(".bselect-search").keyup($.proxy(methods.search, $elem));
-		container.on("click", "li", $.proxy(methods.select, $elem));
+		container.on( "click", "li", $.proxy( methods.select, $elem ) );
 	}
 
-	$.fn.bselect = function(arg) {
-		if (typeof arg === "string" && this[0] && $.isPlainObject($(this[0]).data("bselect"))) {
-			if (methods[arg] !== undefined) {
-				return methods[arg].apply($(this[0]), Array.prototype.slice.call(arguments, 1));
+	$.fn.bselect = function( arg ) {
+		if ( typeof arg === "string" && this[0] && $.isPlainObject( $( this[0] ).data("bselect") ) ) {
+			if ( methods[arg] !== undefined ) {
+				return methods[arg].apply( $( this[0] ), Array.prototype.slice.call( arguments, 1 ) );
 			}
 		}
 
 		return this.each(function() {
-			arg = $.isPlainObject(arg) ? arg : {};
-			arg = $.extend({}, $.bselect.defaults, arg);
+			arg = $.isPlainObject( arg ) ? arg : {};
+			arg = $.extend( {}, $.bselect.defaults, arg );
 
-			setup(this, arg);
+			setup( this, arg );
 		});
 	};
 
 	$.bselect = {
-		defaults : {
-			size : "normal",
-			showOn : "both",
-			clearSearchOnExit : true,
-			minSearchInput    : 0,
-			animationDuration : 300,
-			searchInput       : true,
-			select            : null,
+		defaults: {
+			size: "normal",
+			showOn: "both",
+			clearSearchOnExit: true,
+			minSearchInput: 0,
+			animationDuration: 300,
+			searchInput: true,
+			select: null,
 			synchronizeElement: true
 		},
-		i18n : {
-			selectAnOption : "Select an option"
+		i18n: {
+			selectAnOption: "Select an option"
 		}
 	};
 
-})(jQuery);
+})( jQuery );
